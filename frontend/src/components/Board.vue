bed9c8611de841e92c65705a2a7d6b5382c1dac7
peter <pkh879@gmail.com>
2020-09-21 17:37:29 +0900

    Update board layout

--- frontend/src/components/Board.vue
+++ frontend/src/components/Board.vue
@@ -1,11 +1,21 @@
 <template>
-  <v-container fluid>
+  <v-container>
     <v-layout column>
-      <v-row>
-        <v-avatar color="orange" size="62">
-          <span class="white--text headline">62</span>
-        </v-avatar>
-      </v-row>
+      <v-col class="ma-20 align-center">
+        <v-row>
+          <v-avatar color="orange" size="96">
+            <span class="white--text headline">아이</span>
+          </v-avatar>
+          <v-col>
+            <v-text class="ma-1 font-weight-bold" style="font-size:1.3rem">아이디</v-text>
+            <br />
+            <v-text class="ma-1" style="font-size:1rem">구독자 3명</v-text>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col>
+        <v-divider></v-divider>
+      </v-col>
       <v-flex>
       </v-flex>
     </v-layout>
