1e005dc78ab4511727485208b8474f83b16d560a
박경호 <pkh879@gmail.com>
2020-09-27 15:40:32 +0900

    Add share, edit, delete buttons in leafeditor component

--- frontend/src/components/Leaf.vue
+++ frontend/src/components/Leaf.vue
@@ -1,18 +1,27 @@
 <template>
-<v-container fluid>
-  <v-col class="text-center mt-16" cols="12">
+<v-container class="pa-lg-16 pa-md-16 pa-xs-4 pa-sm-8" style="max-width:900px">
+  <v-col class="text-left mt-16" cols="12">
     <transition name="slide-y-reverse-transition" appear>
       <h1 class="mt-8 mb-4 text-h3" style="font-weight:700">{{this.article.title}}</h1>
     </transition>
     <v-row>
-      <h4 class="subheading mr-4">@{{this.article.author}}</h4>
-      <span v-cloak>{{typeof article.date !== 'undefined' ? this.$Time.dateToFormatKorean(this.article.date) : ''}}</span>
+      <v-row>
+        <h4 class="subheading mr-4">@{{this.article.author}}</h4>
+        <span v-cloak>{{typeof article.date !== 'undefined' ? this.$Time.dateToFormatKorean(this.article.date) : ''}}</span>
+      </v-row>
+      <v-spacer/>
+      <v-row class="d-flex flex-row-reverse">
+        <span class="ma-1"><v-icon>mdi-delete-variant</v-icon> 삭제</span>
+        <span class="ma-1"><v-icon>mdi-pencil-outline</v-icon> 편집</span>
+        <span class="ma-1"><v-icon>mdi-share-variant-outline</v-icon> 공유</span>
+      </v-row>
     </v-row>
   </v-col>
-  <v-col>
+  <v-col style="min-height:200px">
     <span v-html="article.content">
     </span>
   </v-col>
+  <v-divider/>
 </v-container>
 </template>
 
